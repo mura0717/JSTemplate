11d2e24cdcbd921e1604bd7e89291a7ea064e577
Cannabis2013 <MH2012@outlook.dk>
2023-06-13 12:02:20 +0200

    Updated api info

--- api/apiInfo.js
+++ api/apiInfo.js
@@ -2,5 +2,6 @@
     This value should be changed during deployment
  */
 
-export const baseURL = "http://localhost:8080/api"
-// export const baseURL = "https://exam-books-api.azurewebsites.net/api" //deployment
+export const baseURL = "http://localhost:8080/"
+
+export const useLocalData = false
