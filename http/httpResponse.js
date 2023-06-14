fc4dc1fbb49d70dd2c1a05a330b997cf82493a99
mura0717 <mura0717@stud.kea.dk>
2023-06-14 08:25:19 +0200

    update3

--- http/httpResponse.js
+++ http/httpResponse.js
@@ -22,3 +22,33 @@ const handleNotOk = async res => {
     error.fullResponse = errorResponse
     throw error
 }
+
+/**
+ * Small utility function to use in the first "then()" when fetching data from a REST API that supply error-responses as JSON
+ *
+ * Use like this--> const responseData = await fetch(URL,{..}).then(handleHttpErrors)
+ */
+export async function handleHttpErrors(res) {
+    if (!res.ok) {
+      const errorResponse = await res.json();
+      const error = new Error(errorResponse.message);
+      // @ts-ignore
+      error.fullResponse = errorResponse;
+      throw error;
+    }
+    return res.json();
+  }
+  
+  /**
+   * HINT --> USE DOMPurify.santitize(..) to sanitize a full string of tags to be inserted
+   * via innerHTLM
+   * Tablerows are required to be inside a table tag, so use this small utility function to
+   * santitize a string with TableRows only (made from data with map)
+   * DOMPurify is available here, because it's imported in index.html, and as so available in all
+   * your JavaScript files
+   */
+  export function sanitizeStringWithTableRows(tableRows) {
+    let secureRows = DOMPurify.sanitize("<table>" + tableRows + "</table>");
+    secureRows = secureRows.replace("<table>", "").replace("</table>", "");
+    return secureRows;
+  }
--- routing/templateLoading.js
+++ routing/templateLoading.js
@@ -8,6 +8,25 @@ export const renderTemplate = async (pageUrl, contentId) => {
     setContent(template,contentId)
 }
 
+/**
+ * Appends the provided template to the node with the id contentId
+ * @param {*} template The HTML-Template to render
+ * @param {string} contentId
+ */
+/* export function renderTemplate(template, contentId) {
+    const clone = template.content.cloneNode(true);
+    const content = document.getElementById(contentId);
+    content.innerHTML = "";
+    content.appendChild(clone);
+  } */
+
+/**
+ * Loads an external file with an html-template, adds it to the body of your page, and returns the template
+ * The file to be loaded can contain more than one template, but the one that will be returned must
+ * be the first one in the file and this does not require an id
+ * @param {string} page - Path to the file containing the template ('/templates/template.html')
+ * @return {Promise<*>} On succesfull resolvement, the HtmlTemplate found in the file
+ */
 const loadTemplate = async page => {
     const resHtml = await fetch(page).then((r) => {
         if (!r.ok) {
@@ -19,6 +38,7 @@ const loadTemplate = async page => {
     div.innerHTML = resHtml;
     return div.querySelector("template");
 }
+
 
 const setContent = (template,contentId) => {
     const clone = template.content.cloneNode(true);
